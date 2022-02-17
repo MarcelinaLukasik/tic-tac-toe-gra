cb7769436f12089c226bbbc03654a95d135ef2d1
lukasikmarcelina91 <lukasikmarcelina91@gmail.com>
2022-02-17 20:39:31 +0100

    Fixes

--- AI_logic.py
+++ AI_logic.py
@@ -3,14 +3,13 @@ import time
 
 def AI(current_player, game_display):                                   # Wybiera losowe, wolne miejsce na planszy
     while current_player == "O":
-        print("Computer turn...")
+        # print("Computer turn...")
         time.sleep(1)
-        position = random.randint(0, 8)
-        if game_display[position] == "?":
-            game_display[position] = "O"
-            # change_player()
-            if current_player == "X":
-                current_player = "O"
-            else:
-                current_player = "X"
-            return current_player, game_display
+        # for row in range(len(game_display)):
+        row = random.randint(0,2)
+        # for column in range(len(game_display[row])):
+        column = random.randint(0,2)
+        if game_display[row][column] == "?":
+            game_display[row][column] = current_player
+            current_player = "X"
+    return game_display, current_player
